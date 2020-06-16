de1ac5ab6d1e4bf300e451458412d3e28bb2ee6e
atdhebajrami <atdheuaccounts@yahoo.com>
2020-06-16 19:43:34 +0200

    Updates

---
+++ precache-manifest.fdba85bd1bcb3329a913186593d14bbf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "796406472ffa0ed6d3b07d1cd0bfc136",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ae26e7b4f1ae1ac6f6a",
+    "url": "/static/css/main.5626556e.chunk.css"
+  },
+  {
+    "revision": "0ac9b5402598aa8da22f",
+    "url": "/static/js/2.11c32749.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.11c32749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ae26e7b4f1ae1ac6f6a",
+    "url": "/static/js/main.a5756be6.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  },
+  {
+    "revision": "cd4dc25f2ea62cb3733cb9d2e7e23f4a",
+    "url": "/static/media/GitHub.cd4dc25f.png"
+  },
+  {
+    "revision": "6d67e32a85ad5c5642ad31e09719358b",
+    "url": "/static/media/Instagram.6d67e32a.png"
+  },
+  {
+    "revision": "9c43b83f9dbac1d597776746fa997c03",
+    "url": "/static/media/PlayStore.9c43b83f.png"
+  }
+]);
